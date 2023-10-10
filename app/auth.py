cd647887256396f22a94e95cc58159be5baaf38b
labs <emmettwb@gmail.com>
2023-10-10 17:21:04 -0600

    bytes -> str

--- app/auth.py
+++ app/auth.py
@@ -5,7 +5,7 @@ def check_passwd(password: str | bytes) -> bool:
     cursor = db.cursor()
     response = cursor.execute("getLogin 'admin@sms.com'")
     answer = response.fetchone()
-    if type(password) == str:
+    if isinstance(password, str):
         password = password.encode('ascii')
     if answer:
         return bcrypt.checkpw(password, answer[1])
