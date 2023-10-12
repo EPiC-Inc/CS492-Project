e2739f8b7ddc51f4f8739280640546f28e42b2ef
labs <emmettwb@gmail.com>
2023-10-12 03:18:44 -0600

    cleaned up a few things

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ config_parser = ConfigParser()
 config_parser.read("app/configuration.ini")
 config = SimpleNamespace(**config_parser.defaults())
 
-app.secret_key = environ.get("MGMT_SECRET_KEY", AssertionError("Please make sure a secret key is configured with the environment variable MGMT_SECRET_KEY"))
+app.secret_key = environ['MGMT_SECRET_KEY']
 
 @app.context_processor
 def allow_access_in_templates():
--- app/sql.py
+++ app/sql.py
@@ -6,8 +6,7 @@ from os import environ
 server = config.database_url
 database = config.database_name
 username = config.database_user
-password = environ.get('SCHOOL_MANAGEMENT_DB_PASS', 
-                       AssertionError('''Please make sure a database password is configured with the environment variable SCHOOL_MANAGEMENT_DB_PASS'''))
+password = environ['SCHOOL_MANAGEMENT_DB_PASS']
 driver = config.database_driver
 
 db = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
